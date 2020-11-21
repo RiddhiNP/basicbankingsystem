039d5c8d1e2a267779841673f6db6874b5e2a75f
RiddhiNP <riddhipatel980511@gmail.com>
2020-11-22 02:34:21 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	 <link rel="icon" href="b.png">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+
+    <title>Basic System of Banking</title>
+  </head>
+
+  <body style="background-image: url('img_girl.jpg');">
+  
+  
+      <div class="container-fluid" >
+      
+
+      <!-- Activity section -->
+	  <a class="navbar-brand" href="index.php">Welcome to <h1>MY BANK</h1>  </a>
+	  
+	   <br>
+	  <br>
+	  <br>
+            <div class="activity text-center" style="position: absolute;left: 150px;">
+                  
+                    <br>
+	  <br>
+	 
+                    <a href="createuser.php"><button>Create User</button></a>
+                    <br>
+<br>					
+                    
+                    <br>
+                    <a href="transfermoney.php"><button>Make Transaction</button></a>
+                   <br>
+	  <br>
+	  <br>
+                    
+                    <a href="transactionhistory.php"><button>Transaction History</button></a>
+					
+					
+                  
+            </div>
+      </div>
+	  <br>
+	  <br>
+	  <br>
+	 
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	 
+	  <br>
+	  <br>
+	  <br>
+	   <br>
+	  <br>
+	  <br>
+	 
+	  <br>
+	  <br>
+	  <br>
+      <footer class="text-center mt-5 py-2">
+        <p>&copy 2020. Created By <b>Riddhi Patel</b> 
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ sql/bank.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 21, 2020 at 06:34 PM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bank`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `transaction`
+--
+
+CREATE TABLE `transaction` (
+  `sno` int(3) NOT NULL,
+  `sender` text NOT NULL,
+  `receiver` text NOT NULL,
+  `balance` int(8) NOT NULL,
+  `datetime` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `transaction`
+--
+
+INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
+(2, 'Riddhi', 'Sneha', 565, '2020-11-21 22:18:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(3) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(30) NOT NULL,
+  `balance` int(8) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
+(1, 'Shreeji', 'shreeji@gmail.com', 39750),
+(2, 'Ajinkya', 'ajinkya@gmail.com', 70000),
+(3, 'Dhun', 'dhun@gmail.com', 174340),
+(4, 'Prathana', 'prathana@gmail.com', 46500),
+(5, 'Sneha', 'sneha@gmail.com', 75488),
+(6, 'Aradhya', 'Aradhya@gmail.com', 90000),
+(7, 'Nilkanth', 'nilkanth@gmail.com', 80000),
+(8, 'Veda', 'veda@gmail.com', 30700),
+(11, 'Riddhi', 'rid@gmail.com', 22435),
+(12, 'MALHAR', 'malhar@yahoo.com', 15000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `transaction`
+--
+ALTER TABLE `transaction`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `transaction`
+--
+ALTER TABLE `transaction`
+  MODIFY `sno` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
